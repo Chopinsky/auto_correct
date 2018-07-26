23829730c341388e38f7600d100dac855dc7a10b
Jacob Zuo <chopinsky@live.com>
2018-07-25 22:28:10 -0500

    starting the static/speed mode

--- src/static_mode.rs
+++ src/static_mode.rs
@@ -9,7 +9,15 @@ use crossbeam_channel as channel;
 use threads_pool::*;
 
 pub(crate) fn initialize(service: &AutoCorrect) {
+    // if already initialized, calling this function takes no effect
+    if let Err(e) = populate_words_set(&service.config, &service.pool) {
+        eprintln!("Failed to initialize: {}", e);
+        return;
+    }
+}
 
+fn populate_words_set(config: &Config, pool: &ThreadPool) -> Result<(), String> {
+    Ok(())
 }
 
 //TODO: only need table for 1 edit-distance, all ensuing distance can be stacked upon the previous layer
