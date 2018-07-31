829915f595154026cd31f23f9d4c37b899483107
Jacob Zuo <chopinsky@live.com>
2018-07-31 11:33:32 -0500

    finishing up the preproc script

--- resources/pre-processor.py
+++ resources/pre-processor.py
@@ -1,4 +1,5 @@
 import urllib.request
+import time
 from pathlib import Path
 
 URL = "http://www.kilgarriff.co.uk/BNClists/lemma.num"
@@ -26,7 +27,7 @@ def process_words(override=True, skip_score=False, path="lemma.txt", dest_path="
         return
 
     with open(path, 'r') as f:
-        with open(dest_path, 'a') as df:
+        with open(dest_path, 'w') as df:
             lines = f.readlines()
 
             for line in lines:
@@ -58,7 +59,7 @@ def process_n_cut(override=True, cut_level=2, path="freq_50k.txt", dest_path="fr
         return
 
     with open(path, 'r') as f:
-        with open(dest_path, 'a') as df:
+        with open(dest_path, 'w') as df:
             count = 0
             lines = f.readlines()
 
@@ -85,9 +86,9 @@ def process_n_cut(override=True, cut_level=2, path="freq_50k.txt", dest_path="fr
         f.close()
 
 
-def process_n_save(override=True, path="freq_50k.txt", dest_path="freq_50k_precalc.txt"):
-    dest_file = Path(dest_path)
+def process_n_save_reverse(override=True, find_all=True, path="./en-us/freq_50k.txt", dest_path="./en-us/freq_50k_precalc_rev.txt"):
     source_file = Path(path)
+    dest_file = Path(dest_path)
 
     if not source_file.is_file():
         print("Error: can't locate the source file")
@@ -96,10 +97,10 @@ def process_n_save(override=True, path="freq_50k.txt", dest_path="freq_50k_preca
     if dest_file.is_file() and not override:
         return
 
-    dict = {}
+    dict = set()
     with open(path, 'r') as f:
-        with open(dest_path, 'a') as df:
-            print("Info: process begins...")
+        with open(dest_path, 'w') as df:
+            print("Info: process begins...\n")
 
             count = 0
             lines = f.readlines()
@@ -108,22 +109,43 @@ def process_n_save(override=True, path="freq_50k.txt", dest_path="freq_50k_preca
                 if line == "" or line == "word,count\n":
                     continue
 
-                count += 1
                 word_info = line.replace("\n", "").split(',')
 
                 if word_info[0] != "" and word_info[1] != "" and word_info[0] not in dict:
-                    dict[word_info[0]] = word_info[1]
+                    dict.add(word_info[0])
 
-            print(len(dict))
-            for (word, score) in dict.items():
-                neighbors = words_in_one_edit(word, dict)
+            ts = time.gmtime()
+            print(time.strftime("%H:%M:%S", ts), len(dict), "\n")
+
+            rev_dict = {}
+            for word in dict:
+                neighbors = words_in_one_edit(word, dict, find_all)
+                if len(neighbors) == 0:
+                    continue
+                
+                for neighbor in neighbors:
+                    if neighbor in rev_dict:
+                        rev_dict[neighbor] = rev_dict[neighbor] + ";" + word
+                    else:
+                        rev_dict[neighbor] = word
+
+                '''
                 result = word + "^"
-
                 for neighbor in neighbors:
                     if len(neighbor) > 0:
                         result = result + neighbor + ";"
 
                 result = result + "\n"
+                df.write(result)
+                '''
+
+                count += 1
+                if count % 500 == 0:
+                    ts = time.gmtime()
+                    print(time.strftime("%H:%M:%S", ts), " - ", count, "\n")
+
+            for (rev_word, linked) in rev_dict.items():
+                result = rev_word + "^" + linked + "\n"
                 df.write(result)
 
             df.close()
@@ -133,7 +155,63 @@ def process_n_save(override=True, path="freq_50k.txt", dest_path="freq_50k_preca
     return dict
 
 
-def words_in_one_edit(source, dict):
+def process_n_save(override=True, find_all=True, path="./en-us/freq_50k.txt", dest_path="./en-us/freq_50k_precalc.txt"):
+    source_file = Path(path)
+    dest_file = Path(dest_path)
+
+    if not source_file.is_file():
+        print("Error: can't locate the source file")
+        return
+
+    if dest_file.is_file() and not override:
+        return
+
+    dict = set()
+    with open(path, 'r') as f:
+        with open(dest_path, 'w') as df:
+            print("Info: process begins...\n")
+
+            count = 0
+            lines = f.readlines()
+
+            for line in lines:
+                if line == "" or line == "word,count\n":
+                    continue
+
+                word_info = line.replace("\n", "").split(',')
+
+                if word_info[0] != "" and word_info[1] != "" and word_info[0] not in dict:
+                    dict.add(word_info[0])
+
+            ts = time.gmtime()
+            print(time.strftime("%H:%M:%S", ts), len(dict), "\n")
+
+            for word in dict:
+                neighbors = words_in_one_edit(word, dict, find_all)
+                if len(neighbors) == 0:
+                    continue
+                
+                result = word + "^"
+                for neighbor in neighbors:
+                    if len(neighbor) > 0:
+                        result = result + neighbor + ";"
+
+                result = result + "\n"
+                df.write(result)
+
+                count += 1
+                if count % 500 == 0:
+                    ts = time.gmtime()
+                    print(time.strftime("%H:%M:%S", ts), " - ", count, "\n")
+
+            df.close()
+
+        f.close()
+
+    return dict
+
+
+def words_in_one_edit(source, dict, find_all):
     result = set()
     length = len(source)
 
@@ -141,38 +219,42 @@ def words_in_one_edit(source, dict):
         if length > 1:
             # delete...
             delete = source[0:i] + source[i+1:]
-            check_and_add(delete, source, dict, result)
+            check_and_add(delete, source, dict, result, find_all)
 
             # swap...
             if i+2 <= length:
                 swap = source[0:i] + source[i+1] + source[i] + source[i+2:]
-                check_and_add(swap, source, dict, result)
+                check_and_add(swap, source, dict, result, find_all)
 
         for letter in ALPHABET:
             # insert...
             insert = source[0:i] + letter + source[i:]
-            check_and_add(insert, source, dict, result)
+            check_and_add(insert, source, dict, result, find_all)
 
             # replace...
             replace = source[0:i] + letter + source[i+1:]
-            check_and_add(replace, source, dict, result)
+            check_and_add(replace, source, dict, result, find_all)
 
     return result
 
 
-def check_and_add(target, source, dict, result):
-    if target != source and target in dict and target not in result:
-        result.add(target)
+def check_and_add(target, source, dict, result, find_all):
+    if target != source and target not in result:
+        if find_all or target in dict:
+            result.add(target)
 
 
 if __name__ == '__main__':
-    #init_lemma(False)
-    #process_words(dest_path="words2.txt")
-    #process_n_cut(cut_level=0, dest_path="uniq_low.txt")
+    '''Standard words processing
+    init_lemma(False)
+    process_words(dest_path="words2.txt")
+    '''
 
-    dict = process_n_save()
+    '''Processing with different level of dictionary entry numbers
+    process_n_cut(cut_level=0, dest_path="uniq_low.txt")
+    '''
 
-    #result = words_in_one_edit("word", dict)
-    #print(result)
+    #dict = process_n_save()
+    process_n_save_reverse()
 
-    print("\nDone...")
+    print("\nDone...\n")
