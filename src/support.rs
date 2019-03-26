c2fb948c6b692d44be7130a3d9ef257d2cd54a14
Jacob Zuo <chopinsky@live.com>
2019-03-25 22:29:12 -0500

    more trie stuff

--- src/support.rs
+++ src/support.rs
@@ -3,7 +3,7 @@ pub(crate) mod en_us {
     pub(crate) const ALPHABET: [&str;26] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     #[inline(always)]
-    pub(crate) fn get_char_code(rune: char) -> u32 {
-        rune as u32 - 'a' as u32
+    pub(crate) fn get_char_code(rune: char) -> u8 {
+        (rune as u32 - 'a' as u32) as u8
     }
 }
--- src/trie.rs
+++ src/trie.rs
@@ -1,3 +1,5 @@
+use support::en_us;
+
 static mut DICT: Option<Node> = None;
 
 pub(crate) struct Node {
@@ -12,8 +14,46 @@ impl Node {
         Node::default()
     }
 
-    pub(crate) fn contains(word: &str) -> bool {
+    pub(crate) fn insert(_word: &str) {
+        if let Some(root) = dict_mut() {
+            let mut curr = root;
+        }
+    }
 
+    pub(crate) fn contains(word: &str) -> Option<u32> {
+        if let Some(root) = dict_ref() {
+            let mut curr = root;
+
+            'outer: while let Some(rune) = word.chars().next() {
+                // quick reject
+                if curr.occupied == 0 || !curr.check_bit(rune) {
+                    return None;
+                }
+
+                // check which child match the current rune
+                for child in curr.children.as_slice() {
+                    if child.rune == rune {
+                        curr = child;
+                        continue 'outer;
+                    }
+                }
+
+                // shouldn't happen since we've checked the bits, but just be sure
+                return None;
+            }
+
+            return curr.word.as_ref().and_then(|(_, score)| Some(score.clone()));
+        }
+
+        None
+    }
+
+    fn check_bit(&self, rune: char) -> bool {
+        (self.occupied & (1 << en_us::get_char_code(rune))) == 1
+    }
+
+    fn add_bit(&mut self, rune: char) {
+        self.occupied |= (1 << en_us::get_char_code(rune))
     }
 }
 
@@ -22,8 +62,24 @@ impl Default for Node {
         Node {
             rune: '\u{0000}',
             occupied: 0,
-            children: Vec::with_capacity(26),
+            children: Vec::new(),
             word: None,
         }
+    }
+}
+
+#[inline]
+fn dict_ref() -> Option<&'static Node> {
+    unsafe { DICT.as_ref() }
+}
+
+#[inline]
+fn dict_mut() -> Option<&'static mut Node> {
+    unsafe {
+        if DICT.is_none() {
+            DICT.replace(Node::default());
+        }
+
+        DICT.as_mut()
     }
 }
