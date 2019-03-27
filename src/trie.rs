58c39d5d819165b6ac2bdfc999da0e9586b4d7e3
Jacob Zuo <chopinsky@live.com>
2019-03-26 23:16:12 -0500

    more about trie

--- src/trie.rs
+++ src/trie.rs
@@ -1,3 +1,5 @@
+use std::char;
+use channel::Receiver;
 use support::en_us;
 
 static mut DICT: Option<Node> = None;
@@ -14,9 +16,20 @@ impl Node {
         Node::default()
     }
 
-    pub(crate) fn insert(_word: &str) {
+    fn new_with(rune: char, word: Option<(String, u32)>) -> Self {
+        Node {
+            rune,
+            occupied: 0,
+            children: Vec::new(),
+            word,
+        }
+    }
+
+    pub(crate) fn build(rx: Receiver<(String, u32)>) {
         if let Some(root) = dict_mut() {
-            let mut curr = root;
+            for (word, score) in rx.recv() {
+                root.insert(word, score, 0);
+            }
         }
     }
 
@@ -48,12 +61,41 @@ impl Node {
         None
     }
 
+    fn insert(&mut self, word: String, score: u32, index: usize) {
+        let len = word.len();
+        if len == 0 || index >= len {
+            return;
+        }
+
+        let (pos, rune) = find_child_pos(&self.children, &word, index);
+        if pos == self.children.len() {
+            self.add_bit(rune);
+
+            if index == len - 1 {
+                self.children.push(Node::new_with(rune, Some((word, score))));
+                return;
+            } else {
+                self.children.push(Node::new_with(rune, None));
+            }
+        }
+
+        if let Some(child) = self.children.get_mut(pos) {
+            if index == len - 1 {
+                // update child node if this is the whole word
+                child.word = Some((word, score));
+            } else {
+                // insert to the child if not the last character
+                child.insert(word, score, index + 1);
+            };
+        }
+    }
+
     fn check_bit(&self, rune: char) -> bool {
         (self.occupied & (1 << en_us::get_char_code(rune))) == 1
     }
 
     fn add_bit(&mut self, rune: char) {
-        self.occupied |= (1 << en_us::get_char_code(rune))
+        self.occupied |= 1 << en_us::get_char_code(rune)
     }
 }
 
@@ -66,6 +108,26 @@ impl Default for Node {
             word: None,
         }
     }
+}
+
+fn find_child_pos(children: &Vec<Node>, word: &str, index: usize) -> (usize, char) {
+    if let Some(raw) = word.as_bytes().get(index) {
+        if let Some(rune) = char::from_u32(*raw as u32) {
+            let mut index = 0;
+
+            for child in children.iter() {
+                if child.rune == rune {
+                    break;
+                }
+
+                index += 1;
+            }
+
+            return (index, rune);
+        }
+    }
+
+    (children.len(), '\u{0000}')
 }
 
 #[inline]
